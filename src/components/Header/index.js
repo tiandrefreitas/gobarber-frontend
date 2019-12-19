e800f52e1ca0cfb3ebefda016feabe3b6aac42ec
Luis Freitas Da Silva <aluisf@indracompany.com>
2019-12-19 16:51:39 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -29,10 +29,7 @@ export default function Header() {
             </div>
 
             <img
-              src={
-                profile.avatar.url ||
-                'https://api.adorable.io/avatars/50/asdsada.png'
-              }
+              src={'https://api.adorable.io/avatars/50/asdsada.png'}
               alt="Profile"
             />
           </Profile>
